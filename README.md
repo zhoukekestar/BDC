16b1164bbb3d7d49ccc0d93a6207f5c6b170658a
keke <zhoukekestar@163.com>
2016-08-03 14:36:40 +0800

    Added pinyin

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Break-Dark-Clouds plan.
 
 # Repos
 * [pinyin](https://github.com/hotoo/pinyin)
+* 当前拼音基于[百度翻译](http://fanyi.baidu.com/#en/zh/)，很感谢百度翻译能提供这样的接口。如果国外有需要的话，[Google翻译](http://translate.google.cn/?hl=zh-CN&tab=TT)也是不错的选择
 * [网页云课堂](http://study.163.com/course/introduction/232006.htm#/courseDetail)
---
+++ assets/js/pinyin.js
@@ -0,0 +1,51 @@
+!(function(){
+  var proxy = localStorage.getItem('pinyinProxy') || 'https://dev-common.toomao.com/proxy';
+
+  var htmlIt = function(json) {
+    var html = '<p class="pinyin"><u></u><u></u>';
+    for (var i = 0, len = json.trans_result.phonetic.length; i < len; i++) {
+      var t = json.trans_result.phonetic[i];
+      if (t.src_str === '\n') {
+        html += '<br><u></u><u></u>'
+      } else {
+        html += '<u><b>' + t.trg_str + '</b><i>' + t.src_str  + '</i></u>'
+      }
+    }
+    html += '</p>'
+    json.html = html;
+
+  }
+
+  window.pinyin = function(text, callback) {
+
+    if (proxy === '') {
+      return callback && callback('Proxy is null. Please set pinyinProxy by localStorage.getItem("pinyinProxy") at first.')
+    }
+
+    var body = {
+      url: 'http://fanyi.baidu.com/v2transapi',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: 'from=cht&to=zh&query=' + encodeURIComponent(text) + '&transtype=realtime&simple_means_flag=3'
+    }
+    fetch(proxy, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify(body)
+    }).then(response => {
+
+      response.json().then(json => {
+        htmlIt(json);
+        callback && callback(null, json)
+      }).catch (function(e){
+        callback && callback(e)
+      })
+
+    })
+  }
+
+})();
